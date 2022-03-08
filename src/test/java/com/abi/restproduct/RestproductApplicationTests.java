80cc828d6251e1514b50ccaf4651c64e72d67559
hgonzalez22 <hgonzalez.rt@gmail.com>
2022-03-08 07:53:46 -0600

    se agregan prubas de unitarias a la capa del Servicio y Controlador

--- src/test/java/com/abi/restproduct/RestproductApplicationTests.java
+++ src/test/java/com/abi/restproduct/RestproductApplicationTests.java
@@ -1,17 +1,64 @@
 package com.abi.restproduct;
 
 import com.abi.restproduct.controller.ProductController;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.abi.restproduct.service.ProductService;
+import com.abi.restproduct.util.EmailUtility;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ProductController.class)
 class RestproductApplicationTests {
 
-	@Test
-	void contextLoads() {
-		Assertions.assertTrue(true);
-	}
+	@Autowired                           
+    private MockMvc mockMvc;  
+                                                 
+    @MockBean                           
+    private ProductService productService; 
+                                       
+
+//
+//	@Test
+//	void contextLoads() {
+////		Assertions.assertTrue(true);
+//	
+//	}
+	
+    
+    
+    @DisplayName("Test Spring Controller Layer, Get Products")
+    @Test
+    public void getProductAPI() throws Exception {
+    	this.mockMvc.perform(MockMvcRequestBuilders.get("/api/products"))
+    	.andExpect(status().isOk());
+    }
+	
+    @DisplayName("Test Spring Controller Layer, Delete Products")
+    @Test
+    public void deleteProductAPI() throws Exception{
+    	this.mockMvc.perform(MockMvcRequestBuilders.delete("/api/products/{id}", 1))
+    	.andExpect(status().isOk());
+    }
+    
+    @DisplayName("Test Spring Service Layer, find()")
+    @Test
+    public void findProduct() {
+    	Assertions.assertTrue(!(productService.find(1).isEmpty()));
+    }
+    
 
 }
